fc185b8afc7df81247f797f0666c8f768f873bd3
conniechz <644608020011@dpu.ac.th>
2022-06-14 11:58:44 +0700

    first commit

---
+++ Rectangle.py
@@ -0,0 +1,6 @@
+#รับ6----
+width = float(input("Enter Width : "))
+length = float(input("Enter Length : "))
+
+area = width * length
+print(f'Rectangle Area : {area}')
---
+++ person.py
@@ -0,0 +1,46 @@
+class Person:
+    country = 'Thailand' #Class variable
+    def __init__(self,name,gender,proffession,study) -> None:
+        self.name = name #instance variable
+        self.gender = gender #instance variable
+        self.profession = proffession #instance variable
+        self.study2 = study #instance variable
+
+    def work(self):
+        print(f'{self.name} is working as a {self.profession}')
+
+    def study(self):
+        print(f'{self.name} studies for {self.study2} hour(s)')
+    
+    def show(self):
+        print(f'Name : {self.name}  Gender : {self.gender}  Profession : {self.profession} Study : {self.study2} hour(s)')
+
+person_obj = Person('Jessa','Male','Software Engineer',10)
+person_obj.work()
+person_obj.study()
+person_obj.show()
+
+John = Person('John','Male','Professional',15)
+John.work()
+John.study()
+John.show()
+
+Lisa = Person('Lisa','Female','Korean Singer',15)
+Lisa.work()
+
+print(f'Class Variable : {Person.country}')
+print(f'Instance Variable : {Lisa.country}')
+
+#assign value
+Lisa.country = 'Korea'
+print('-----------')
+print(f'Class Variable : {Person.country}')
+print(f'Instance Variable : {Lisa.country}')
+print(f'Instance Variable : {John.country}')
+
+#assign Class variable
+Person.country = 'England'
+print('-----------')
+print(f'Class Variable : {Person.country}')
+print(f'Instance Variable : {Lisa.country}')
+print(f'Instance Variable : {John.country}')
